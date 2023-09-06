01b75f22ee8767b0f4c1d9ddc16a3bb46c7ffbfa
Omar Asael Hernandez Montiel <51717542+omarasael1980@users.noreply.github.com>
2023-09-05 21:50:21 -0700

    agregando frontend, instalacion de tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+ 
+function App() {
+  
+  return (
+    <>
+      <div className="bg-red-400">
+        <h1 className="bg-blue-600">Desde app</h1>
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
